5270c8c305ae6a79ee02e09d72772273035ed12a
竹田陽平 <Yohei-Matsutan>
2022-03-19 17:37:30 +0900

    add controllers

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,7 @@
+class UsersController < ApplicationController
+	def main
+	end
+
+	def index
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  root to: 'homes/top'
+
+  get 'stamps/index'
+  get 'stamps/show'
+  get 'stamps/edit'
+  get 'stamps/update'
+  get 'stamps/destroy'
+  get 'homes/top'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
