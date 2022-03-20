ceeb9edc1cde5b652d53d51853cb0861f036923f
竹田陽平 <Yohei-Matsutan>
2022-03-20 16:56:47 +0900

    add authorizing admin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,4 +12,20 @@ class UsersController < ApplicationController
 			redirect_to users_main_path
 		end
 	end
+
+	def show
+		@user = User.find(params[:id])
+	end
+
+	def authorize
+		if current_user.is_admin
+			user = User.find(params[:id])
+			tf = !user.is_admin
+			user.update(is_admin: tf)
+			redirect_to users_path
+		else
+			flash[:failure] = '管理者として認証されていません。申請をお願いします。'
+			redirect_to users_main_path
+		end
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
 
+  devise_for :users
   get 'users/main'
-  resources :users, only: [:index]
+  patch 'users/:id/authorize' => 'users#authorize', as: 'authorize'
+  resources :users, only: [:index, :show]
 
   post 'stamps/clock_in', as: 'clock_in'
   patch 'stamps/clock_out', as: 'clock_out'
@@ -12,7 +14,5 @@ Rails.application.routes.draw do
   get 'stamps/edit'
   get 'stamps/update'
   get 'stamps/destroy'
-  get 'homes/top'
-  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
