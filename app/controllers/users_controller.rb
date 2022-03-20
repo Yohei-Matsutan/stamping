b71e2b978c34d9a768d165f3338a964cb0aaf016
竹田陽平 <Yohei-Matsutan>
2022-03-20 22:40:24 +0900

    add csv output

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,4 @@
+require 'csv'
 class UsersController < ApplicationController
 	def main
 		@user = current_user
@@ -16,6 +17,13 @@ class UsersController < ApplicationController
 	def show
 		@user = User.find(params[:id])
 		@stamps = @user.stamps
+
+		respond_to do |format|
+			format.html
+			format.csv do |csv|
+			  send_stamps_csv(@stamps)
+			end
+		end
 	end
 
 	def authorize
@@ -28,5 +36,17 @@ class UsersController < ApplicationController
 			flash[:failure] = '管理者として認証されていません。申請をお願いします。'
 			redirect_to users_main_path
 		end
+	end
+
+	def send_stamps_csv(stamps)
+		csv_data = CSV.generate(row_sep: "\r\n", encoding:Encoding::CP932)  do |csv|
+		  header = %w(名前 出勤時間 退勤時間 勤務時間)
+		  csv << header
+		  stamps.each do |stamp|
+			values = [stamp.user.name, stamp.in_time, stamp.out_time, stamp.work_hour]
+			csv << values
+		  end
+		end
+		send_data(csv_data, filename: "stamps.csv")
 	end
 end
